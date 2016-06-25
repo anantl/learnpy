9db42dcfc5b1c10ee368d4a030bd2aeeda1bd3b2
Ananth L <ananth.l@hotmail.com>
2016-06-25 15:22:46 +0530

    End of intro into modules

--- modules_demo/module1_demo.py
+++ modules_demo/module1_demo.py
@@ -1,6 +1,11 @@
+# module1 can be imported freely since it is there in the same folder
 import module1
 
+# Calling a function from module1
 module1.demo_format("Jumbo jack")
 
+# printing a variable that is from the module that was imported
 print(module1.ananth)
+
+#Passing a variable from the imported module to a function in the module itself
 module1.demo_format(module1.ananth)
