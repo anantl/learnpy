6047bcb817dccd0cb53ec2d1c0a6c221fdcbae7b
Ananth L <ananth.l@hotmail.com>
2016-06-25 15:17:16 +0530

    Sample module classes and implementation

---
+++ modules_demo/module1.py
@@ -0,0 +1,4 @@
+def demo_format(a = "Hello"):
+	print("Modified "+a)
+
+ananth = "Ananth"
---
+++ modules_demo/module1_demo.py
@@ -0,0 +1,6 @@
+import module1
+
+module1.demo_format("Jumbo jack")
+
+print(module1.ananth)
+module1.demo_format(module1.ananth)
